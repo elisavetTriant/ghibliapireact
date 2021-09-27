f179c40ba3a81ad27c77ca015bd37d38e36f0d69
elisavetTriant <elitriant@gmail.com>
2021-09-27 14:40:57 +0300

    Pulling YouTube Video Data from API
    *API: https://github.com/elisavetTriant/ghibli-movies-api

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import { searchMovies, requestMovies, requestMovie, assignModal} from './StateContainer/reducers'
+import { searchMovies, requestMovies, requestMovie, requestMovieVideos, assignModal} from './StateContainer/reducers'
 import './index.css';
 import Home from './containers/Home';
 import Movie from './containers/Movie';
 //import reportWebVitals from './reportWebVitals';
 
 const logger = createLogger();
-const rootReducers = combineReducers({requestMovies, requestMovie, searchMovies, assignModal});
+const rootReducers = combineReducers({requestMovies, requestMovie, requestMovieVideos, searchMovies, assignModal});
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
 //const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
 
